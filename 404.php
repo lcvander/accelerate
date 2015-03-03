444c782446dbcf7450778e0f3b47c508b35495db
lcvander <leah.vanderveldt@gmail.com>
2015-03-04 08:54:14 +1000

    Commit March 4

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for 404 page
+*/
+
+get_header(); ?>
+<div id="primary" class="site-content">
+	<div id="content" role="404">
+
+<h2> Whoops, Took a Wrong Turn...</h2>
+<h4> Sorry, this page no longer exists, never existed 
+or has been moved. We feel like complete jerks for totally misleading you.
+<br> Feel free to take a look around our <a href="http://localhost:40432/blog/">blog</a> or some of our featured 
+<a href = "http://localhost:40432/case-studies/">work</a>. </h4>
+
+	</div><!-- #content -->
+</div>
+
+<?php get_footer(); ?>
